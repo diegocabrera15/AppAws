018deef9f19a86302bedf86c5b94699cf4f072f4
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-10-24 18:13:43 -0500

    Edit file config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_CyHos2GCp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4qi7t1c6k6n7sh5o5na1483vmi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
